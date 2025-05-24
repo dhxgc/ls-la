c62dbbc3e91992e33e5733bec5f2f202902d6ee5
dhxgc <timakuvaev93131@gmail.com>
2025-05-25 00:27:40 +0300

    FTP deploy

---
+++ scripts/ftp-deploy.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# === !!!CHANGE NETWORKS!!! === 
+ALLOWED_NETS="10.0.0.0/24 192.168.1.0/24"
+
+apt-get install vsftpd anonftp -y
+
+# Backups
+cp /etc/xinetd.conf{,.bak}
+cp /etc/vsftpd.conf{,.bak}
+
+# New configs
+echo "#
+# Simple configuration file for xinetd
+#
+# Some defaults, and include /etc/xinetd.d/
+
+defaults
+{
+        log_type = SYSLOG authpriv info
+        log_on_success = PID HOST DURATION
+        log_on_failure = HOST
+        instances = 100
+        per_source = 5
+        only_from = ${ALLOWED_NETS}
+}
+
+includedir /etc/xinetd.d
+" > /etc/xinetd.conf
+
+
+echo "listen_ipv6=NO
+anonymous_enable=NO
+local_enable=YES
+write_enable=YES
+local_umask=066
+dirmessage_enable=NO
+use_localtime=YES
+xferlog_enable=YES
+connect_from_port_20=YES
+#secure_chroot_dir=/var/run/vsftpd/empty
+pam_service_name=vsftpd
+rsa_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem
+rsa_private_key_file=/etc/ssl/private/ssl-cert-snakeoil.key
+ssl_enable=NO
+
+chroot_local_user=YES
+allow_writeable_chroot=YES
+pam_service_name=vsftpd
+" > /etc/vsftpd.conf
+
+service xinetd restart
+systemctl enable xinetd
